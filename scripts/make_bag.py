f003b21da734580d3c59612de7738b5e5f64609a
Colin Keil <keil.c@northeastern.edu>
2024-02-14 19:17:54 -0500

    script for making rosbag from list of images

---
+++ scripts/make_bag.py
@@ -0,0 +1,43 @@
+import sys
+import rosbag
+import time
+import cv2
+import numpy as np
+import os
+import glob
+import cv_bridge
+import rospy 
+
+from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import Header
+
+def get_image(fname):
+    #ensures 8 bit
+    tstamp = os.path.basename(fname)[:-4]
+    return cv2.imread(fname, 0), tstamp
+
+if __name__ == "__main__":
+
+    # fname = "/media/colin/box_data/ir_data/nuance_data/kri_day_2/cam_3/matlab_clahe2/1689804908243000031.png"
+    path = "/media/colin/box_data/ir_data/nuance_data/kri_day_2/cam_3/matlab_clahe2/"
+    topic = "/boson_camera_array/cam_3/image_raw"
+
+    files = sorted(glob.glob(os.path.join(path, "*.png")))
+
+    bridge = cv_bridge.CvBridge()
+
+    with rosbag.Bag(os.path.join(path,"out_bag.bag"), "w") as outbag:
+        for i, f in enumerate(files):
+            print(f)
+            image, stamp = get_image(f)
+            # tstamp = rospy.Time(secs=int(stamp[:10]), nsecs=int(stamp[10:]))
+            image_message = bridge.cv2_to_imgmsg(image, header=Header(seq = i))
+            image_message.header.stamp.nsecs = int(stamp[10:])
+            image_message.header.stamp.secs = int(stamp[:10])
+            outbag.write(topic, image_message, t=image_message.header.stamp)
+
+    # for file in files:
+    #     print(file)
+    
+    # im, stamp = get_image(fname)
+    # print(stamp)
